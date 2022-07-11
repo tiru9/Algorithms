9fe28be115144d8ee66da107058ebfb3504ccc52
tiru9 <53965480+tiru9@users.noreply.github.com>
2022-07-11 20:10:37 +0530

    dp day 2

---
+++ src/DP/fibonacci/EqualSubSetSum.java
@@ -0,0 +1,114 @@
+package DP.fibonacci;
+
+/*
+*
+* Given a set of positive numbers,
+* find if we can partition it into two subsets such that the sum of elements in both the subsets is equal.
+*
+* ex:   Input: {1, 2, 3, 4}
+        Output: True
+        Explanation: The given set can be partitioned into two subsets with equal sum: {1, 4} & {2, 3}
+* */
+
+
+public class EqualSubSetSum {
+    public static void main(String args[]) {
+        int arr[] = new int[]{1, 2, 3, 4, 6};//true
+        int totalSum = 0;
+        for (int num : arr) {
+            totalSum += num;
+        }
+
+
+
+        if (totalSum % 2 == 0){
+            int s = totalSum/2;
+            System.out.println("hasEqualSubSetSumRecursion " + hasEqualSubSetSumRecursion(arr, s, 0));
+            Boolean[][] memo = new Boolean[arr.length][totalSum / 2];
+            System.out.println("hasEqualSubsetSumMemoization " + hasEqualSubsetSumMemoization(arr, memo, s, 0));
+
+            boolean[][] table = new boolean[arr.length][s + 1];
+            System.out.println("hasEqualSubsetSumTabulation " + hasEqualSubsetSumTabulation(arr, table,s, 0));
+
+        }else {
+            System.out.println("Equal subset sum not possible");
+        }
+    }
+
+    private static Boolean hasEqualSubSetSumRecursion(int[] arr,int sum, int index) {
+
+        if (sum == 0)
+            return true;
+        if(index >= arr.length)
+            return false;
+
+        if (arr[index] <= sum) {
+            // recursive call after choosing the number at the currentIndex
+            // if the number at currentIndex exceeds the sum, we shouldn't process this
+            if (hasEqualSubSetSumRecursion(arr, sum - arr[index], index+1))
+                return true;
+        }
+
+        // recursive call after excluding the number at the currentIndex
+        return hasEqualSubSetSumRecursion(arr, sum, index+1);
+
+        /*
+         * Time Complexity: O(2^n) , n is size of array
+         * Space Complexity: o(n), memory which is used to store the recursion stack
+         */
+    }
+
+    private static Boolean hasEqualSubsetSumMemoization(int[] arr, Boolean[][] memo, int sum, int index) {
+
+        if (sum == 0)
+            return true;
+        if(index >= arr.length)
+            return false;
+
+        if (memo[index] == null) {
+            // recursive call after choosing the number at the currentIndex
+            // if the number at currentIndex exceeds the sum, we shouldn't process this
+            if (hasEqualSubSetSumRecursion(arr, sum - arr[index], index+1)) {
+                memo[index][sum] = true;
+                return true;
+            }
+        }
+
+        // recursive call after excluding the number at the currentIndex
+        return hasEqualSubSetSumRecursion(arr, sum, index+1);
+
+        /*
+         * Time Complexity: O(n * s) , n is size of array
+         * Space Complexity: o(n * s), sum is half total of all numbers
+         */
+    }
+
+    private static Boolean hasEqualSubsetSumTabulation(int[] arr, boolean[][] table, int sum, int index) {
+
+        // populate the sum=0 column, as we can always have '0' sum without including any element
+        for(int i = 0; i < arr.length; i++){
+            table[i][0] = true; //if sum is zero partition is possible without considering any elements for sum
+        }
+
+        for(int s = 1; s <= sum; s++){
+            table[0][s] = arr[0] == s ? true : false;
+        }
+
+        for(int i = 1; i < arr.length; i++){
+            for(int s = 1; s <= sum; s++){
+                // if we can get the sum 's' without the number at index 'i'
+                if(table[i-1][s]) {
+                    table[i][s] = table[i-1][s]; // copy value from above row in case of possible
+                } else if (s >= arr[i]) { // else if we can find a subset to get the remaining sum
+                    table[i][s] = table[i-1][s - arr[i]];
+                }
+            }
+        }
+        return table[arr.length - 1][sum];
+
+        /*
+         * Time Complexity: O(n * s) , n is size of array
+         * Space Complexity: o(n * s), sum is half total of all numbers
+         */
+    }
+}
--- src/DP/fibonacci/canSumOrTargetSum.java
+++ src/DP/fibonacci/canSumOrTargetSum.java
@@ -15,8 +15,7 @@ public class canSumOrTargetSum {
 
     // Returns true if there is a subset
     // of set[] with sum equal to given sum
-    static boolean canSumRecursive(int num[], int sum , int n)
-    {
+    static boolean canSumRecursive(int num[], int sum, int n) {
         //System.out.println("Remaining target :: "+ sum);
         // Base Cases
         if (sum == 0)
@@ -27,49 +26,48 @@ public class canSumOrTargetSum {
         // If last element is greater than
         // sum, then ignore it
         if (num[n - 1] > sum)
-            return canSumRecursive(num, sum,n - 1);
+            return canSumRecursive(num, sum, n - 1);
 
         /* else, check if sum can be achieved by any of the following
             (a) including the last element
             (b) excluding the last element */
-        return canSumRecursive(num, sum, n - 1)
-                || canSumRecursive(num, sum - num[n - 1], n - 1);
+        return canSumRecursive(num, sum - num[n - 1], n - 1) || canSumRecursive(num, sum, n - 1);
 
         /*
-        *  Time complexity O(n^m)
-        *   where m: sum, n is size of array
-        */
+         *  Time complexity O(n^m)
+         *   where m: sum, n is size of array
+         */
     }
 
-    public static boolean canSumMemoization(int[] num, boolean[] mem, int sum, int n){
-        if(sum == 0)
+    public static boolean canSumMemoization(int[] num, boolean[] mem, int sum, int n) {
+        if (sum == 0)
             return true;
 
         if (n < 0)
             return false;
 
-        if(mem[n])
+        if (mem[n])
             return mem[n];
 
         // If last element is greater than
         // sum, then ignore it
-        if (sum < 0){//when current element not fit into to sum
+        if (sum < 0) {//when current element not fit into to sum
             mem[n] = false;
             return mem[n];
         }
 
-        if(n > 0){
-            mem[n] = canSumMemoization(num, mem, sum,n - 1)
-                    || canSumMemoization(num, mem,sum - num[n-1], n - 1 );
+        if (n > 0) {
+            mem[n] = canSumMemoization(num, mem, sum, n - 1)
+                    || canSumMemoization(num, mem, sum - num[n - 1], n - 1);
         }
 
         return mem[n];
 
         /*
-        * Space complexity O(m)
-        * Time complexity O(m * n)
-        * where m: target sum, n is size of array
-        * */
+         * Space complexity O(m)
+         * Time complexity O(m * n)
+         * where m: target sum, n is size of array
+         * */
     }
 
     public static int canSumSubsetCount(int[] num, int count, int sum, int n) {
@@ -89,25 +87,54 @@ public class canSumOrTargetSum {
         //count when  excluded n-1 th element and sum up
         if (n > 0) {
             count = count + canSumSubsetCount(num, count, sum, n - 1) +
-                    + canSumSubsetCount(num, count, sum - num[n - 1], n - 1); //exclude current + include current
+                    +canSumSubsetCount(num, count, sum - num[n - 1], n - 1); //exclude current + include current
         }
         return count;
     }
 
-        public static void main(String args[])
-    {
-       int num[] = new int[]{4, 5, 2, 5, 3, 6, 1};
-       int sum = 9;//true
-       //int num[] = new int[]{4, 5, 2};
-       //int sum = 10;//false
+    private static boolean canSumTabulation(int[] num, boolean[][] table, int sum) {
+        // step1: fill first columns with true as sum zero is possible with out taking any number from array
+        for (int i = 0; i <= num.length; i++)
+            table[0][i] = true;
 
-        int n = num.length;
-        System.out.println("SumRecursive, Found a subset :: "+ canSumRecursive(num, sum, n));
-        boolean[] mem = new boolean[n];
-        System.out.println("canSumMemoization, Found a subset :: "+ canSumMemoization(num, mem, sum, n-1));
+        //step2: fill first rows with false as no sum possible with considering zero
+        //So java following step not required as default primitive boolean values are false
+        // for(int j = 0; j <= sum; j++)
+        //     table[j][0] = true;
 
-        int subSetCount =  0;
-        System.out.println("canSumSubsetCountMemoization, Found a subset :: "+ canSumSubsetCount(num,  subSetCount, sum, n-1));
+        // step3: Fill table for possible sums until target
+        for (int i = 1; i <= num.length; i++)
+            for (int j = 1; j <= sum; j++) {
+                //assign the current row values with above row, this can be true or false based on previous possibilities
+                table[i][j] = table[i][j - 1];
+
+               /*check if current sum is greater than current value from input array
+                 a) if greater compare current table[i][j] with remainder sum column from previous row and make xor
+                    i.e sum is possible with current and previous values
+                 b) else continue filling
+                */
+                if(i >= num[i-1])
+                    table[i][j] =  table[i][j] ||  table[i-num[i-1]][j-1];
+            }
+        //Final sum possibility is calculated at right-bottom corner
+        return table[num.length][sum];
     }
 
+    public static void main(String args[]) {
+        int num[] = new int[]{4, 5, 2, 5, 3, 6, 1};
+        int sum = 9;//true
+        //int num[] = new int[]{4, 5, 2};
+        //int sum = 10;//false
+
+        int n = num.length;
+        System.out.println("SumRecursive, Found a subset :: " + canSumRecursive(num, sum, n));
+        boolean[] mem = new boolean[n];
+        System.out.println("canSumMemoization, Found a subset :: " + canSumMemoization(num, mem, sum, n - 1));
+
+        boolean table[][] = new boolean[n + 1][sum + 1];
+        System.out.println("canSumTabulation, Found a subset :: " + canSumTabulation(num, table, sum));
+
+        int subSetCount = 0;
+        System.out.println("canSumSubsetCountMemoization, Found a subset :: " + canSumSubsetCount(num, subSetCount, sum, n - 1));
+    }
 }
